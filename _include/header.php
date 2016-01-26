8228288952e363868f26c272b12d5bcd61c491f2
Erozbliz <erozbliz@hotmail.com>
2016-01-26 17:49:02 +0100

    Maj
    Important

---
+++ _include/header.php
@@ -0,0 +1,33 @@
+<!-- Navigation -->
+        <nav class="navbar navbar-default navbar-fixed-top animated slideInDown" role="navigation">
+            <div class="container ">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php">
+                        <img src="images/logo.png" width="60px" alt="">
+                    </a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li >
+                            <a href="index.php" > Liste des MOOCs</a>
+                        </li>
+                        <li>
+                            <a href="index.php">A Propos</a>
+                        </li>
+                        <li>
+                            <a href="inscription1.php"> <span class="glyphicon glyphicon-log-in" aria-hidden="true"></span> Connexion</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
