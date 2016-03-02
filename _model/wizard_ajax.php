0f800ae2913bccc18f141ad8c3044339a04f3a4a
Erozbliz <erozbliz@hotmail.com>
2016-03-02 13:03:25 +0100

    ajax

--- _model/wizard_ajax.php
+++ _model/wizard_ajax.php
@@ -1,10 +1,10 @@
 <?php
-include "connect.inc.php";  /// Connection bdd
+include "../_include/connect.inc.php"; //connexion bdd
 $meschoix=$_POST["dataForm"]; //Mes choix
 $solutions=$_POST["dataForm2"]; //Les solutions
 $idm=$_POST["dataidm"]; //id mooc
 $idc=$_POST["dataidc"]; //id chap
-$ide=$_POST["dataide"]; //id exo
+$tabide=$_POST["dataide"]; //id exo
 
 /*
 $meschoix='["Monsieur Jeson Dupont","Jean Nicolas"]';
@@ -17,16 +17,42 @@ $solutions=str_replace(',]',"]",$solutions);
 
 
 
+function insertFaitToBDD($id_user,$score){
+	try { 
+		$requete_prepare= $bdd->prepare("INSERT INTO faire(score,id_user,id_exercice) VALUES('$score', '$id_user', '$valPseudo')"); // on prépare notre requête
+		$requete_prepare->execute();
+		var_dump($requete_prepare);
+		echo "->OK insertUsertoBDD";
+
+	} catch (Exception $e) { 
+  		echo $e->errorMessage();
+  		echo "->erreur";
+	}
+
+}
+
 //echo 'wizard ajax';
 //echo 'wizard_ajax.php';
 //$callback = $meschoix."<br>".$solutions;
 //echo $callback;
 //echo json_encode($callback);
+/*
+if ((isset($_SESSION['id_user'])) && (!empty($_SESSION['id_user']))){
+	//ok session
+	$id_user=$_SESSION['id_user'];
+	$score = 100;
+	insertFaitToBDD($id_user,$score);
+}
+*/
+
 
 if($meschoix == $solutions){
-	echo 'CORRECT <br> les réponses sont :'.$solutions."<br>info : ".$idm." ".$idc." ".$ide;
+	echo 'CORRECT <br> les réponses sont :'.$solutions."<br>info : ".$idm." ".$idc." ".$tabide;
+
 }else{
-	echo 'INCORRECT <br> les réponses sont :'.$solutions."<br>| ".$idm." ".$idc." ".$ide;
+	echo 'INCORRECT <br> les réponses sont :'.$solutions."<br>| ".$idm." ".$idc." ".$tabide;
+
+
 }
 exit();
 ?>
